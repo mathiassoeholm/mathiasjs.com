2535aeaf8ec8d0f40156d3d05ef68a9cb9bae78d
Mathias Søholm <mss@trifork.com>
2019-12-15 19:51:14 +0100

    Styled subscribe component

--- src/gatsby-theme-blog/components/subscribe.js
+++ src/gatsby-theme-blog/components/subscribe.js
@@ -1,25 +1,80 @@
-import React, { useState } from "react"
-import addToMailchimp from "gatsby-plugin-mailchimp"
+/** @jsx jsx */
+import { jsx } from "theme-ui"
 
 export const Subscribe = () => {
-  const [email, setEmail] = useState("")
-
   const onSubmit = () => {
     window.open("https://buttondown.email/soeholm", "popupwindow")
   }
 
   return (
-    <form
-      action="https://buttondown.email/api/emails/embed-subscribe/soeholm"
-      method="post"
-      target="popupwindow"
-      onsubmit={onSubmit}
-      class="embeddable-buttondown-form"
+    <div
+      sx={{
+        backgroundColor: "subscribeBackground",
+        color: "subscribeText",
+        display: "flex",
+        flexDirection: "column",
+        padding: 2,
+        borderRadius: 10,
+        alignItems: "center",
+        maxWidth: 350,
+        margin: "auto",
+      }}
     >
-      <label for="bd-email">Enter your email</label>
-      <input type="email" name="email" id="bd-email" />
-      <input type="hidden" value="1" name="embed" />
-      <input type="submit" value="Subscribe" />
-    </form>
+      <h3
+        sx={{
+          margin: 1,
+          fontFamily: "heading",
+        }}
+      >
+        Get new blog posts by email
+      </h3>
+      <form
+        sx={{
+          display: "grid",
+          gridTemplateColumns: "1fr",
+          gridGap: 1,
+          width: "100%",
+          fontFamily: "heading",
+        }}
+        action="https://buttondown.email/api/emails/embed-subscribe/soeholm"
+        method="post"
+        target="popupwindow"
+        onSubmit={onSubmit}
+      >
+        <label htmlFor="bd-email">Email</label>
+        <input
+          sx={{
+            fontSize: 2,
+            border: "none",
+            padding: 1,
+            borderRadius: 4,
+            fontFamily: "heading",
+          }}
+          type="email"
+          name="email"
+          id="bd-email"
+          placeholder="alice@mail.com"
+        />
+        <input type="hidden" value="1" name="embed" />
+        <input
+          sx={{
+            fontSize: 2,
+            borderRadius: 4,
+            backgroundColor: "subscribeButton",
+            color: "subscribeText",
+            border: "none",
+            fontFamily: "heading",
+            padding: 1,
+            marginTop: 1,
+            fontWeight: "bold",
+            "&:hover": {
+              backgroundColor: "subscribeButtonHover",
+            },
+          }}
+          type="submit"
+          value="Subscribe"
+        />
+      </form>
+    </div>
   )
 }
