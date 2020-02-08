b8d88d9081c542e8d73c0cccd51d167fff4b2146
Mathias Søholm <mss@trifork.com>
2020-02-08 20:08:53 +0100

    Fix layout issue with code samples on iOS

--- src/gatsby-theme-blog/gatsby-plugin-theme-ui/styles.js
+++ src/gatsby-theme-blog/gatsby-plugin-theme-ui/styles.js
@@ -5,4 +5,11 @@ export default merge(defaultStyles, {
   a: {
     textDecoration: "none",
   },
+  pre: {
+    span: {
+      // By default this is set to "inline-block" by node_modules/@theme-ui/prism/src/index.js
+      // this causes issues on Safari for iOS with some of the code samples.
+      display: "inline",
+    },
+  },
 })
