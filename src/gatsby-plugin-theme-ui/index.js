159bad710078c0a80e648343b56297d59d9ad6f4
Mathias Søholm <mathiassoeholm@gmail.com>
2020-05-19 20:59:04 +0200

    Scrap old stuff

--- src/gatsby-plugin-theme-ui/index.js
+++
@@ -1,9 +0,0 @@
-import merge from "deepmerge"
-import defaultTheme from "gatsby-theme-blog/src/gatsby-plugin-theme-ui/index"
-import "typeface-open-sans"
-
-export default merge(defaultTheme, {
-  fonts: {
-    heading: `Open Sans, Montserrat, sans-serif`,
-  },
-})
--- src/gatsby-theme-blog/gatsby-plugin-theme-ui/prism.js
+++
@@ -1,6 +0,0 @@
-import baseTheme from "@theme-ui/prism/presets/vs-dark.json"
-
-export default {
-  ...baseTheme,
-  backgroundColor: "#13102b",
-}
